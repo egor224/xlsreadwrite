989866f8aaca91a16be3d6f7381818b10c5df3b7
chappi <gchappi@gmail.com>
2010-01-09 11:47:11 +0100

    Fix CRAN CHECK break (#65)

--- tests/runRUnitTests.R
+++ tests/runRUnitTests.R
@@ -1,27 +1,22 @@
 #
 # runRUnitTests.R
 #
-# - executes ~all tests in '../inst/RUnitTests'
-# - intended to be run from CHECK only
-#   (getwd() is supposed to be in <PATH_TO_PKG>.Rcheck/tests)
+# Executes tests in '../inst/RUnitTests'.
 
-wd <- getwd()
-pkgdir <- dirname( wd )
-pkg <- sub( "\\.Rcheck$", '', basename( pkgdir ) )
-shlib <- paste( pkgdir, "/", pkg, "/libs/", pkg, .Platform$dynlib.ext, sep = "" )
-stopifnot( file.exists( shlib ) )
+pkg <- "xlsReadWrite"
+shlib <- system.file("libs",
+		          paste(pkg, .Platform$dynlib.ext, sep=""),
+              package = pkg)
+stopifnot(file.exists(shlib))
 
 if (file.info( shlib )$size < 20000) {
-	  # message not printed on the console, only appears in the log
-  message( "tests not executed (cran placeholder shlib)" )
-
+ ## message not printed on the console, only appears in the log
+ message( "tests not executed (cran placeholder shlib)" )
 } else if (require( "RUnit", quietly = TRUE )) {
-  rutdir <- file.path( pkgdir, pkg, "RUnitTests" )
-  stopifnot( file.exists( rutdir ), file.info( rutdir )$isdir )
-  source( file.path( rutdir, "runner.R") )
-  runTestsForCHECK( rutdir )
-
+ rutdir <- system.file("RUnitTests", package = pkg)
+ stopifnot(file.exists(rutdir), file.info(rutdir)$isdir)
+ source(system.file("RUnitTests", "runner.R", package = pkg))
+ runTestsForCHECK(rutdir)
 } else {
-  stop( "tests not executed ('RUnit' package is required)" )
+ stop( "tests not executed ('RUnit' package is required)" )
 }
-
